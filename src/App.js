0aa2b25a9f4fa2c4d52e6dbcd931475e1d958af0
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-02-14 05:23:40 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,68 +1,38 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 import { fetchCats } from './actions/catActions'
- 
-class App extends Component {
- 
-  componentDidMount() {
-    console.log(this.props)
-    this.props.fetchCats()
-  }
- 
-  render() {
-    console.log(this.props.catPics) 
-=======
-import { connect } from "react-redux";
-import { fetchCats } from "./actions/catActions";
+import CatList from './CatList'
 
 class App extends Component {   
-
+  
   componentDidMount() {
-    console.log(this.props);
-    this.props.fetchCats();
+    this.props.fetchCats()
   }
   
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading) {
+      return <div>Loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
+
   render() {
-    console.log(this.props.catPics)
->>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
     return (
       <div className="App">
         <h1>CatBook</h1>
-        {/* missing component */}
+        {this.handleLoading()}
       </div>
     );
   }
 }
-<<<<<<< HEAD
- 
-const mapStateToProps = state => {
-=======
 
-const mapStateToProps = (state) => {
->>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
+const mapDispatchToProps = state => {
   return {
     catPics: state.cats,
     loading: state.loading
   }
 }
-<<<<<<< HEAD
- 
-const mapDispatchToProps = dispatch => {
-=======
 
-const mapDispatchToProps = (dispatch) => {
->>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
-  return {
-    fetchCats: () => dispatch(fetchCats())
-  }
-}
-<<<<<<< HEAD
-export default connect(mapStateToProps, mapDispatchToProps)(App)
-=======
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
->>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
+export default connect(mapDispatchToProps, { fetchCats })(App)
--- src/CatList.js
+++ src/CatList.js
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 class CatList extends Component {
   render() {
     return (
-    
+      <div>
+        {
+          this.props.catPics.map(cat => 
+            <img key={ cat.id } src={ cat.url } alt={ cat.id } />  
+          )
+        }
+      </div>
     );
   }
 }
