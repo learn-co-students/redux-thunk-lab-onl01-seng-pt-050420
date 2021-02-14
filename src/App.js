ee95e2864191ce6d5953da89e82c816a0d81beac
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-02-14 02:20:46 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { connect } from 'react-redux';
 import { fetchCats } from './actions/catActions'
  
@@ -11,6 +12,20 @@ class App extends Component {
  
   render() {
     console.log(this.props.catPics) 
+=======
+import { connect } from "react-redux";
+import { fetchCats } from "./actions/catActions";
+
+class App extends Component {   
+
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchCats();
+  }
+  
+  render() {
+    console.log(this.props.catPics)
+>>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
     return (
       <div className="App">
         <h1>CatBook</h1>
@@ -19,17 +34,35 @@ class App extends Component {
     );
   }
 }
+<<<<<<< HEAD
  
 const mapStateToProps = state => {
+=======
+
+const mapStateToProps = (state) => {
+>>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
   return {
     catPics: state.cats,
     loading: state.loading
   }
 }
+<<<<<<< HEAD
  
 const mapDispatchToProps = dispatch => {
+=======
+
+const mapDispatchToProps = (dispatch) => {
+>>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
   return {
     fetchCats: () => dispatch(fetchCats())
   }
 }
+<<<<<<< HEAD
 export default connect(mapStateToProps, mapDispatchToProps)(App)
+=======
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+>>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,10 +1,17 @@
 export const fetchCats = () => {
   return (dispatch) => {
+<<<<<<< HEAD
     dispatch({ type: 'LOADING_CATS'})
     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
       return response.json()
     }).then(responseJSON => {
       dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
     })
+=======
+    dispatch({ type: "LOADING_CATS" })
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+      .then(response => response.json())
+      .then(responseJSON => dispatch({ type: "ADD_CATS", cats: responseJSON.images }))
+>>>>>>> 76d9776789db8e5b32f34b536bccaff9d2f7672c
   }
 }
